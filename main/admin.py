998319ed5f6a30da3e3ca275376e1d833544fe45
leon-rdo <leonardoabreu1212@gmail.com>
2023-08-07 11:31:25 -0300

    chore: aplica melhorias na área de administração do site.

--- main/admin.py
+++ main/admin.py
@@ -4,15 +4,36 @@ from main.models import *
 class PagamentoInline(admin.TabularInline):
     model = Pagamento
     extra = 0
+    readonly_fields = ['jogador']
 
+@admin.register(Partida)
 class PartidaAdmin(admin.ModelAdmin):
+    ordering = ['-data']
+    list_filter = ['data']
+    list_display = ['__str__', 'sorteada']
+    
     inlines = [PagamentoInline]
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_filter = ('tipo',)
+    ordering = ['nome_jogador']
+    search_fields = ['nome_jogador']
+    list_display = ['nome_jogador', 'tipo', 'posicao']
+    list_filter = ['tipo']
+    readonly_fields = ['idade', 'gols_marcados', 'pontos', 'date_joined', 'last_login']
+    
+@admin.register(Patrocinador)
+class PatrocinadorAdmin(admin.ModelAdmin):
+    ordering = ['nome']
+    list_display = ['nome', 'link']
+    
+@admin.register(Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'texto']
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Partida, PartidaAdmin)
-admin.site.register(Patrocinador)
-admin.site.register(Configuracao)
-admin.site.register(Noticia)
+@admin.register(Configuracao)
+class ConfiguracaoAdmin(admin.ModelAdmin):
+    list_display = ['email_diretor', 'telefone_diretor', 'chave_pix', 'alerta_mensagem']
+    
+admin.site.site_header = 'Administração da FFBVI'
+admin.site.index_title = 'FFBVI'
