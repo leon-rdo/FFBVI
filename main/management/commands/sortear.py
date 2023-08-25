4d4d7a7a9ed4f2bc75d79416b13d456b2c95466b
leon-rdo <leonardoabreu1212@gmail.com>
2023-08-25 13:47:16 -0300

    refactor: uso de match-case ao invés de if-else no comando sortear

--- main/management/commands/sortear.py
+++ main/management/commands/sortear.py
@@ -28,48 +28,45 @@ class Command(BaseCommand):
             shuffle(goleiros)
             shuffle(jogadores)
 
-            # Verifica se há 3 goleiros na lista
-            if len(goleiros) == 3:
-                # Distribui os goleiros nos três times
-                times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
-                shuffle(times)
-                times[0].set([goleiros[0]])
-                times[1].set([goleiros[1]])
-                times[2].set([goleiros[2]])
-                self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, três são goleiros.'))
-                
-            # Verifica se há 2 goleiros na lista
-            elif len(goleiros) == 2:
-                # Atribui os goleiros aleatoriamente a dois dos times
-                times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
-                shuffle(times)
-                times[0].set([goleiros[0]])
-                times[1].set([goleiros[1]])
-                self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, dois são goleiros.'))
-                
-            # Verifica se há apenas um goleiro na lista
-            elif len(goleiros) == 1:
-                # Atribui o único goleiro aleatoriamente a um dos times
-                times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
-                time_escolhido = choice(times)
-                time_escolhido.set([goleiros[0]])
-                self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, apenas um goleiro.'))
-                    
-            # Verifica se há mais de 3 goleiros na lista
-            elif len(goleiros) > 3:
-                # Distribui os goleiros nos três primeiros times
-                times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
-                shuffle(times)
-                times[0].set([goleiros[0]])
-                times[1].set([goleiros[1]])
-                times[2].set([goleiros[2]])
+            # Verifica a quantidade de goleiros e os distribui entre os times
+            match len(goleiros):
+                case 3:
+                    # Caso haja três, coloca um em cada time
+                    times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
+                    shuffle(times)
+                    times[0].set([goleiros[0]])
+                    times[1].set([goleiros[1]])
+                    times[2].set([goleiros[2]])
+                    self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, três são goleiros.'))
 
-                # Distribui os goleiros restantes aleatoriamente nos times
-                for goleiro in goleiros[3:]:
+                case 2:
+                    # Caso haja dois, os coloca em dois times randomicamente
+                    times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
+                    shuffle(times)
+                    times[0].set([goleiros[0]])
+                    times[1].set([goleiros[1]])
+                    self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, dois são goleiros.'))
+
+                case 1:
+                    # Caso haja apenas um, o coloca em um time aleatório
+                    times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
                     time_escolhido = choice(times)
-                    time_escolhido.add(goleiro)
-                        
-                self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, e mais de quatro goleiros.'))
+                    time_escolhido.set([goleiros[0]])
+                    self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, apenas um goleiro.'))
+
+                case n if n > 3:
+                    # Caso haja mais de três, coloca os três primeiros, um em cada time e o resto aleatoriamente
+                    times = [partida.time_verde, partida.time_vermelho, partida.time_azul]
+                    shuffle(times)
+                    times[0].set([goleiros[0]])
+                    times[1].set([goleiros[1]])
+                    times[2].set([goleiros[2]])
+
+                    for goleiro in goleiros[3:]:
+                        time_escolhido = choice(times)
+                        time_escolhido.add(goleiro)
+                    
+                    self.stdout.write(self.style.WARNING(f'Há {len(relacionados)} relacionados, e mais de quatro goleiros.'))
 
             # Atribua os jogadores restantes aos times, de acordo com a lotação
             for jogador in jogadores:
