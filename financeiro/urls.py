bbf1c4a32424435502c2891998bd18824e2b3cce
leon-rdo <leonardoabreu1212@gmail.com>
2023-08-26 18:49:39 -0300

    feat: cria página de confirmar pagamentos

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -4,5 +4,7 @@ from .views import *
 app_name = 'financeiro'
 urlpatterns = [
     path('', IndexView.as_view(), name='menu_financeiro'),
+    path('pagamentos-pendentes/', PagamentosPendentesView.as_view(), name='pagamentos_pendentes'),
+    path('confirmar-pagamento/<int:pk>', ConfirmarPagamentoView.as_view(), name='confirmar_pagamento'),
 ]
 
--- financeiro/views.py
+++ financeiro/views.py
@@ -1,5 +1,8 @@
-from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from django.views import View
+from django.views.generic import TemplateView, ListView
 from main.models import Pagamento, Partida
+from django.contrib import messages
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -10,3 +13,19 @@ class IndexView(TemplateView):
         context["partidas"] = Partida.objects.all()
         return context
     
+class PagamentosPendentesView(ListView):
+    model = Pagamento
+    template_name = "confirmar_pagamento.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pagamentos"] = Pagamento.objects.filter(confirmado=False)
+        return context
+
+class ConfirmarPagamentoView(View):
+    def get(self, request, pk):
+        pagamento = Pagamento.objects.get(pk=pk)
+        pagamento.confirmado = True
+        pagamento.save()
+        messages.success(self.request, f'Pagamento de {pagamento.jogador.nome_jogador} confirmado!')
+        return redirect('financeiro:pagamentos_pendentes')
