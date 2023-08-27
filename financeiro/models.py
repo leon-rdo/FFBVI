f6cf0900dd29f4c21220c3fe5f06bdf2410543a7
leon-rdo <leonardoabreu1212@gmail.com>
2023-08-27 11:47:25 -0300

    refactor: aumenta quantidade de caracteres da coluna descricao no modelo Saida

--- financeiro/models.py
+++ financeiro/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 class Saida(models.Model):
 
-    descricao = models.CharField(_("Descrição da saída:"), max_length=50)
+    descricao = models.CharField(_("Descrição da saída:"), max_length=255)
     valor = models.DecimalField(_("Valor da saída:"), max_digits=5, decimal_places=2)
     data = models.DateField(_("Data"), auto_now_add=True)
 
