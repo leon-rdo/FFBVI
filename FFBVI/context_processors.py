53a61e4d37936c4ec6ca6b2ca5c75e595c60fddd
leon-rdo <leonardoabreu1212@gmail.com>
2023-08-27 15:58:29 -0300

    adiciona modelo de configurações ao context processors

--- FFBVI/context_processors.py
+++ FFBVI/context_processors.py
@@ -1,4 +1,8 @@
-from main.models import Patrocinador
+from main.models import Configuracao, Patrocinador
+
+def configuracoes(request):
+    configuracoes = Configuracao.objects.first()
+    return {'configuracoes': configuracoes}
 
 def patrocinadores(request):
     patrocinadores = Patrocinador.objects.all()
