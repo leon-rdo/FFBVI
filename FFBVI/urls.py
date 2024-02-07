f63283f22ed418e1f767cd4b0c22dc9c1f2665d1
Leonardo Costa <leonardoabreu1212@gmail.com>
2024-02-07 15:29:47 -0300

    refactor: add condicional to serve media files

--- FFBVI/urls.py
+++ FFBVI/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path('', include('main.urls')),
     path('financeiro/', include('financeiro.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
